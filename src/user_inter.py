889e880e20273e75a17d558b57c36e77d920b338
gelena <helenatcitok@gmail.com>
2023-12-18 17:49:46 +0000

    fixing mistakes

--- src/user_inter.py
+++ src/user_inter.py
@@ -1,51 +1,36 @@
 from src.classes_api import HeadHunterAPI, SuperJobAPI
 from src.savedvac import JSONSaver
-from src.vacancies import Vacancy
-from utils import filter_vacancies, get_top_vacancies, print_vacancies, sort_vacancies
+from src.utils import get_top_vacancies, print_vacancies, sort_vacancies, filter_vacancies
 from dotenv import load_dotenv
 
 load_dotenv()
-
-hh_api = HeadHunterAPI()
-superjob_api = SuperJobAPI()
-json_saver = JSONSaver("vacancies.json")
 
 
 def user_interaction():
     # Получение выбора пользователя
     platform = input("Выберите платформу (Headhunter/Superjob): ").lower()
-    print(f"DEBUG: Приведенная платформа: {platform}")
 
-    if platform != "headhunter" and platform != "superjob":
+    if platform not in ("headhunter", "superjob"):
         print(f"Некорректная платформа {platform}. Выход из программы.")
         return
-    api = None
+
     # Инициализация соответствующего API
     if platform == "headhunter":
         api = HeadHunterAPI()
-        hh_api.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
-
     elif platform == "superjob":
         api = SuperJobAPI()
-    else:
-        print("Некорректная платформа. Выход из программы.")
-        return
-    search_query = input("Введите поисковый запрос: ")
-    # Получение вакансий
-    vacancies = api.get_vacancies(search_query)
+        return api
 
-    # Создание экземпляра класса для сохранения вакансий
-    json_saver = JSONSaver("vacancies.json")
+    search_query = input("Введите поисковый запрос: ")
 
     # Взаимодействие с пользователем
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
     filter_words = input(
-        "Введите ключевые слова описания характеристик работника для фильтрации вакансий (через пробел): ").split()
+        "Введите ключевые слова описания характеристик работника для фильтрации вакансий (через пробел): "
+    ).lower().split()
 
     # Фильтрация вакансий
-    filtered_vacancies = filter_vacancies(vacancies, filter_words)
+    filtered_vacancies = filter_vacancies(api.get_vacancies(search_query), filter_words)
 
     if not filtered_vacancies:
         print("Нет вакансий, соответствующих заданным критериям.")
@@ -66,7 +51,4 @@ def user_interaction():
         json_saver = JSONSaver("vacancies.json")
         for vacancy in top_vacancies:
             json_saver.add_vacancy(vacancy)
-
-
-if __name__ == "__main__":
-    user_interaction()
+        json_saver.save_to_file()
